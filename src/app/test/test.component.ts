8938a5fd481355a4681f8d21425e466d5994f98f
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-05-12 12:57:35 +0545

    class binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,16 +6,49 @@ import { Component, OnInit } from '@angular/core';
   selector: '[app-test]',   // use as attribute
   templateUrl: './test.component.html',  // points to file that content html
   // template: '<div>Inline template</div>',
+  // template: `<h2 class="text-success">anish</h2>
 
-  // styleUrls: ['./test.component.css']
+
+  // `,
+
+  styleUrls: ['./test.component.css']
   // styles:[''] inline css
+  // styles: [`
+  // .text-success{
+  //   color-green;
+
+  // }
+  // .text-danger{
+  //   color:red;
+  // }
+  // .text-special{
+  //   font-style:italic;
+  // }
+
+  // `]
 
 })
 export class TestComponent implements OnInit {
-  public name = "code with me anish";
-  public myId = "testId";
-  public isDisabled = false;
+  // properties
+  // public myId = "testId";
+  // public isDisabled = false;
   // public siteUrl = window.location.href;  // creating new property 
+  // public sucessClass = "text-success";
+  public name = "code with me anish";
+  public sucessClass = "text-success";
+  public hasError = true;
+  public isSpecial = true;
+  // public hasError = false;
+
+  public messgaeClasses = {
+    "text-success": !this.hasError,
+    "text-danger": this.hasError,
+    "text-special": this.isSpecial
+  }
+
+
+
+
 
   constructor() { }
 
